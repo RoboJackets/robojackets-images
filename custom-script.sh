c5430b6fbed1242ff57274b44a824b5a302c46d7
Jay Kamat <jaygkamat@gmail.com>
2016-07-23 15:33:21 -0700

    Fix a couple errors in the setup script

--- custom-script.sh
+++ custom-script.sh
@@ -8,7 +8,7 @@ set -eux
 # For example:
 # yum install -y curl wget git tmux firefox xvfb
 sudo apt-get update
-sudo apt-get install -f git
+sudo apt-get install -y git
 cd
 git clone https://github.com/RoboJackets/robocup-software.git
 git clone https://github.com/RoboJackets/robocup-firmware.git
@@ -18,6 +18,6 @@ sudo apt-get install -y qtcreator
 # Sublime
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
-sudo apt-get install sublime-text-installer
+sudo apt-get install -y sublime-text-installer
 
 # TODO actually run ubuntu-setup
