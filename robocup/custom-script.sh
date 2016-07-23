c2bd15db7c81f3d622f9e534abaaae53547694be
Jay Kamat <jaygkamat@gmail.com>
2016-07-23 14:45:22 -0700

    Start to add robocup-software and other tools

--- robocup/custom-script.sh
+++ robocup/custom-script.sh
@@ -7,3 +7,17 @@ set -eux
 #
 # For example:
 # yum install -y curl wget git tmux firefox xvfb
+sudo apt-get update
+sudo apt-get install -f git
+cd
+git clone https://github.com/RoboJackets/robocup-software.git
+git clone https://github.com/RoboJackets/robocup-firmware.git
+git clone https://github.com/RoboCup-SSL/ssl-refbox.git
+sudo apt-get install -y qtcreator
+
+# Sublime
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+
+# TODO actually run ubuntu-setup
