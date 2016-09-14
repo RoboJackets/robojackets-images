40911c112f9324bbd76df2300901329800833912
Jay Kamat <jaygkamat@gmail.com>
2016-09-14 01:41:05 -0400

    Use distro's guest addition plugins

--- robocup/script/virtualbox.sh
+++ robocup/script/virtualbox.sh
@@ -7,15 +7,16 @@ if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     # Assuming the following packages are installed
     # apt-get install -y linux-headers-$(uname -r) build-essential perl
     # apt-get install -y dkms
+    apt-get install -y virtualbox-guest-{dkms,x11}
 
-    VBOX_VERSION=$(cat /home/${SSH_USER}/.vbox_version)
-    mount -o loop /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
-    sh /mnt/VBoxLinuxAdditions.run
-    umount /mnt
-    rm /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso
-    rm /home/${SSH_USER}/.vbox_version
-
-    if [[ $VBOX_VERSION = "4.3.10" ]]; then
-        ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
-    fi
+    # VBOX_VERSION=$(cat /home/${SSH_USER}/.vbox_version)
+    # mount -o loop /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+    # sh /mnt/VBoxLinuxAdditions.run
+    # umount /mnt
+    # rm /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso
+    # rm /home/${SSH_USER}/.vbox_version
+    #
+    # if [[ $VBOX_VERSION = "4.3.10" ]]; then
+    #     ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+    # fi
 fi
